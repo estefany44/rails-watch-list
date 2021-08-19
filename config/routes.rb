df49869bb0f7fc3ad098f97e632db451a7e01fd5
estefany romo <estefanyromoalvarez@gmail.com>
2021-08-19 10:53:46 -0400

    actividad 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to 'list#index'
+  resources :lists, except: [:edit, :update] do
+  resources :bookmarks, only: [:new, :create]
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
+  resources :bookmarks, only: :destroy
 end
